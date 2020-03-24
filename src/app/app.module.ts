eba26942b67434fa89341fcde5da9923f71fceb6
Walace Rodrigues <walace.maia@totvs.com.br>
2020-03-23 22:31:56 -0300

    feat(master): crud users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,64 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { appRoutingModule } from './app.routing';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AlertComponent } from './_components';
+import {TranslateModule, TranslateLoader, TranslateService} from '@ngx-translate/core';
+import { CoreModule } from './core/core.module';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader'
+
+export function createTranslateLoad(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule,
+        //CoreModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoad,
+                deps: [HttpClient]
+            }
+        })
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AlertComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        { provide: LOCALE_ID, useValue: 'pt' },
+
+        // provider used to create fake backend
+        //fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+constructor(private translateService: TranslateService) {
+    translateService.addLangs(['en', 'pt']);
+        translateService.setDefaultLang['en'];
+        const browserLang = translateService.getBrowserLang();
+        translateService.use(browserLang.match(/en|pt/) ? browserLang : 'en');
+}
+
+};
